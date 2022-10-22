d7fb5e30585ac483705db909b475e086ceb20611
TVHstudio <93575862+TVHstudio@users.noreply.github.com>
2022-10-22 22:12:02 +0700

    update1

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req,res) => {
-  res.send('Hello sir')
+app.use(express.static(__dirname + '/public'));
+let expressHbs = require('express-handlebars');
+let hbs = expressHbs.create({
+    extname : 'hbs',
+    defaultLayout : 'layouts',
+    layoutsDir: __dirname + '/views/layouts',
+    partialsDir:__dirname + '/views/partials'
 });
+app.engine('hbs',hbs.engine);
+app.set('view engine', 'hbs');
 
-app.get('/abc',(req,res) => {
-  res.send('Hello from abc')
+app.get('/', (req,res) => {
+    res.render('index');
+});
+ app.get('/:page', (req,res) => {
+     let banners = {
+         blog : 'Our blog',
+         category : 'Shop Category',
+         cart : 'Shopping Cart',
+         checkout : 'Checkout',
+         confirmation : 'Confirmation',
+         contact : 'Contact',
+         register : 'Register',
+         login : 'login',
+     };
+     let page = req.params.page;
+     res.render(page, {banner: banners[page]});
+ });
+
+app.get('/blog', (req,res) => {
+  res.render('blog');
 });
 
 app.set('port', process.env.PORT || 3000);
