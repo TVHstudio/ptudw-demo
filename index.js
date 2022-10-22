f5a3e49addfb87f99616a143bce9562f8ec93989
TVHstudio <93575862+TVHstudio@users.noreply.github.com>
2022-10-22 22:01:30 +0700

    scn commit

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/', (req,res) => {
   res.send('Hello sir')
 });
 
+app.get('/abc',(req,res) => {
+  res.send('Hello from abc')
+});
+
 app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), ()=> {
     console.log(`Server is running on port ${app.get('port')}`);
