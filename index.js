21aa5167bc0b38ac22ff92d7d837f88548c4774c
TVHstudio <93575862+TVHstudio@users.noreply.github.com>
2022-10-23 10:15:11 +0700

    ch

--- index.js
+++ index.js
@@ -11,9 +11,58 @@ app.engine('hbs',expressHbs.engine({
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
+app.get('/:page', (req,res) => {
+    let banners = {
+        blog : 'Our blog',
+        category : 'Shop Category',
+        cart : 'Shopping Cart',
+        checkout : 'Checkout',
+        confirmation : 'Confirmation',
+        contact : 'Contact',
+        register : 'Register',
+        login : 'login',
+    };
+    let page = req.params.page;
+    res.render(page, {banner: banners[page]});
+});
+
 app.get('/',(req,res) => {
     res.render('index')
 })
+app.get('/blog', (req,res) => {
+    res.render('blog');
+});
+app.get('/cart', (req,res) => {
+    res.render('cart');
+});
+app.get('/category', (req,res) => {
+    res.render('category');
+});
+app.get('/checkout', (req,res) => {
+    res.render('checkout');
+});
+app.get('/confirmation', (req,res) => {
+    res.render('confirmation');
+});
+app.get('/contact', (req,res) => {
+    res.render('contact');
+});
+app.get('/register', (req,res) => {
+    res.render('register');
+});
+app.get('/login', (req,res) => {
+    res.render('login');
+});
+app.get('/single-blog', (req,res) => {
+    res.render('single-blog');
+});
+app.get('/single-product', (req,res) => {
+    res.render('single-product');
+});
+app.get('/tracking-order', (req,res) => {
+    res.render('tracking-order');
+});
+
 
 app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), ()=> {
