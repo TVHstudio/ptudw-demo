adee5503c4e2b5098d7cc5277c4141ccbaa75894
TVHstudio <93575862+TVHstudio@users.noreply.github.com>
2022-10-23 10:10:46 +0700

    sync test

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
+const expressHbs = require('express-handlebars');
 
+app.engine('hbs',expressHbs.engine({
+    layoutsDir: __dirname + '/views/layouts',
+    partialsDir:__dirname + '/views/partials',
+    extname : 'hbs',
+    defaultLayout : 'layouts',
+}));
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
-app.set('/',(req,res) =>{
-    res.send('Hello from my website')
-} );
+
+app.get('/',(req,res) => {
+    res.render('index')
+})
+
 app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), ()=> {
     console.log(`Server is running on port ${app.get('port')}`);
