701d6e6debff740e7b8072e6ceda4213893cdb84
TVHstudio <20810212@student.hcmus.edu.vn>
2022-10-26 16:32:03 +0700

    list view

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -6,8 +6,12 @@ router.get('/', (req,res) => {
     productController.getTopProducts()
     .then(topProducts => {
         res.locals.topProducts = topProducts;
+        return productController.getAllProducts();
+    })
+    .then(products => {
+        res.locals.products = products;
         res.render('category');
-    });
+    })
 });
 
 module.exports = router;
