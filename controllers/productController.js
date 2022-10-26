cd21675cb04fc8028d5fd2d3195a376fe9f46da4
TVHstudio <20810212@student.hcmus.edu.vn>
2022-10-26 16:30:10 +0700

    list view

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,30 +3,40 @@ const models = require('../models');
 const Product = models.Product;
 
 controller.getAllProducts = () => {
-    return new Promise((resolve, reject) => {
-        Product.findAll()
-        .then(data => resolve(data))
-        .catch(error => reject(new Error(error)));
-    } )
+    // return new Promise((resolve, reject) => {
+    //     Product.findAll({
+    //         include : [
+    //             {model : models.Category}
+    //         ]                       
+    //     })
+    //     .then(data => resolve(data))
+    //     .catch(error => reject(new Error(error)));
+    // } )
+
+    return Product.findAll({
+        include: [
+            { model: models.Category }
+        ]
+    })
 }
 
 controller.getTopProducts = () => {
     return new Promise((resolve, reject) => {
         Product.findAll({
             limit: 12,
-            order:[
-                ['overallReview','DESC']
+            order: [
+                ['overallReview', 'DESC']
             ]
         })
-        .then(data => {
-            let topProducts = [];
-            while (data.length) {
-                topProducts.push(data.splice(0, 3));
-            }
-            resolve(topProducts);
-        })
-        .catch(error => reject(new Error(error)));
-    } )
+            .then(data => {
+                let topProducts = [];
+                while (data.length) {
+                    topProducts.push(data.splice(0, 3));
+                }
+                resolve(topProducts);
+            })
+            .catch(error => reject(new Error(error)));
+    })
 }
 
 
